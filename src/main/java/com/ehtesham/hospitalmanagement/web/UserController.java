3b4aee9eaca5fa1acce6131412896a7f6c9973d8
ehteshamshahzad <ehteshamshahzad@hotmail.com>
2020-02-29 14:55:56 +0500

    Added the ability to get list of patients for a doctor

--- src/main/java/com/ehtesham/hospitalmanagement/web/UserController.java
+++ src/main/java/com/ehtesham/hospitalmanagement/web/UserController.java
@@ -9,6 +9,7 @@ import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.util.List;
 import java.util.Optional;
 
 @RestController
@@ -29,14 +30,26 @@ public class UserController {
         return userService.getAllUsers();
     }
 
-    @GetMapping("/{user_id}")
-    public Optional<User> getUserByUserId(@PathVariable Long user_id) {
-        return userService.findUserById(user_id);
+    @GetMapping("/user_id/{user_id}")
+    public ResponseEntity<?> getUserByUserId(@PathVariable Long user_id) {
+        Optional<User> user = userService.findUserById(user_id);
+        return new ResponseEntity<Optional<User>>(user, HttpStatus.OK);
+    }
+
+    @GetMapping("/user_name/{username}")
+    public ResponseEntity<?> getUserByUserId(@PathVariable String username) {
+        User user = userService.findUserByUsername(username);
+        return new ResponseEntity<User>(user, HttpStatus.OK);
     }
 
     @PostMapping("/add_patient")
-    public ResponseEntity<?> addPatient(@Valid @RequestBody User user, BindingResult result){
+    public ResponseEntity<?> addPatient(@Valid @RequestBody User user, BindingResult result) {
         User doctor = userService.assignPatientToDoctor(user);
         return new ResponseEntity<User>(doctor, HttpStatus.CREATED);
+    }
+
+    @GetMapping("/patients_list/{username}")
+    public List<User> getListOfPatients(@PathVariable String username) {
+        return userService.findUserByUsername(username).getListOfPatients();
     }
 }
